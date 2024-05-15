0829b75a5de4e1010eb806da3a517214d382efa2
Frank Luongo <frank@frankluongo.com>
2024-05-15 19:32:32 -0400

    fix links

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import { resolve } from "path";
 import vituum from "vituum";
 import handlebars from "@vituum/vite-plugin-handlebars";
 
-const dirs = ["components", "partials"];
-const partialDirectory = dirs.map((dir) => resolve(__dirname, dir));
-
 export default {
   plugins: [
     vituum(),
     handlebars({
       root: resolve(__dirname, "src"),
-      partialDirectory,
     }),
   ],
 };
